1d7bc4a1f4ed5d76a7897c740ed5132962e3340e
Igor Svistun <igorekha2003@icloud.com>
2022-05-07 12:08:37 +0300

    fix

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,27 +1,22 @@
 package main
 
 import (
+	"github.com/AXlIS/go-shortener/internal/config"
 	"github.com/AXlIS/go-shortener/internal/handler"
 	"github.com/AXlIS/go-shortener/internal/server"
 	"github.com/AXlIS/go-shortener/internal/service"
 	store "github.com/AXlIS/go-shortener/internal/storage"
-	"github.com/joho/godotenv"
 	"log"
-	"os"
 )
 
 func main() {
-	if err := godotenv.Load(); err != nil {
-		log.Fatalf("Error loading env variables: %s", err.Error())
-	}
-
 	storage := store.NewStorage()
 	services := service.NewService(storage)
 	handlers := handler.NewHandler(services)
 
 	s := new(server.Server)
 
-	if err := s.Start(os.Getenv("SERVER_ADDRESS"), handlers.InitRoutes()); err != nil {
+	if err := s.Start(config.GetEnv("SERVER_ADDRESS", "8080"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Error occured while running http server: %s", err.Error())
 	}
 }
